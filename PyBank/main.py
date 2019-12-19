adad8939b4982d8049c4bc33963867c74da914f8
Justin Boggs <jboggs1@gmail.com>
2019-12-19 13:01:53 -0600

    "working"

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,24 +5,28 @@ import os
 import csv
 
 #Path to collect bank data
-bank_csv = os.path.join("..", "PyBank", "budget_data.csv")
+bank_csv = os.path.join("budget_data.csv")
 
 date = []
 profit = []
-change = []
 
 #Open and read the csv
 with open(bank_csv, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvfile)
-    print(f"{header}")
-
+    
+    for row in csvreader:
+        date.append(row[0])
+        profit.append(int(row[1]))
+    
+total_months = len(date)
+total_profit = sum(profit)
 
 
 print("Financial Analysis")
 print("------------------")
-print(f"Total Months: ")
-print(f"Total: ")
+print(f"Total Months: " + str(total_months))
+print(f"Total: " + str(total_profit))
 print(f"Average Change: ")
 print(f"Greatest Increase in Profits: ")
 print(f"Greatest Decrease in Profits: ")
